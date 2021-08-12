e90b035c339ead8c43544e98d3cf3d19b3666558
chans <papajuju123@naver.com>
2021-08-12 20:04:38 +0900

    feat: introspection

--- src/server.js
+++ src/server.js
@@ -12,6 +12,7 @@ const apollo = new ApolloServer({
   resolvers,
   typeDefs,
   playground: true,
+  introspection: true,
   context: async ({ req }) => {
     return { loggedInUser: await getUser(req.headers.token), protectResolver };
   },
